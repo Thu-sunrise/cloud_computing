0d1f916155eea50e689c64c947f4099c4b1f6b06
JohnJohn1603 <kiet.tran05tgkpy@hcmut.edu.vn>
2025-11-01 22:32:42 +0700

    my auth.middleware recommendation

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,8 +1,8 @@
 import { TokenService } from "../services/token.service.js";
 
 export const requireAuth = async (req, res, next) => {
-  const session = req.cookies.session;
-  const persistent = req.cookies.persistent;
+  const session = req.cookies.sessionToken;
+  const persistent = req.cookies.persistentToken;
   try {
     const payload = TokenService.verifySessionToken(session);
     if (payload) {
@@ -35,7 +35,7 @@ export const requireAuth = async (req, res, next) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "strict",
-    maxAge: 60 * 60 * 1000,
+    // maxAge: 60 * 60 * 1000,
   });
 
   req.user = payload;
@@ -55,4 +55,60 @@ export const requireRole = (...allowedRoles) => {
 
     next();
   };
+};
+
+//--------------------My-Recommendation------------------------
+export const requireAuth2 = async (req, res, next) => {
+  const session = req.cookies.sessionToken;
+  const persistent = req.cookies.persistentToken;
+  try {
+    const payload = TokenService.verifySessionToken(session);
+    if (payload) {
+      req.user = payload;
+      return next();
+    } else {
+      return res.status(401).json({ message: "Invalid Session Token" });
+    }
+  } catch (err) {
+    // If Sessions Token expired, keep checking on Persistent Token
+    if (err.name === "TokenExpiredError") {
+      console.log("[SYSTEM] Session Token expired, getting new Session Token...");
+      try {
+        const doc = await TokenService.verifyPersistentToken(persistent);
+        if (!doc) {
+          return res.status(401).json({ message: "Invalid persistent token" });
+        }
+
+        //Rotate Session Token inside Persistent Token Condition
+        const { newSessionToken, payload } = await TokenService.rotateSessionToken(doc.userId);
+
+        if (!newSessionToken) {
+          return res.status(401).json({ message: "User not found" });
+        }
+
+        res.cookie("sessionToken", newSessionToken, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: "strict",
+          // maxAge: 60 * 60 * 1000,
+        });
+
+        req.user = payload;
+
+        return next();
+      } catch (err) {
+        // In case Persistent Token expired, request user to refresh them all.
+        if (err.name === "TokenExpiredError") {
+          console.log("[SYSTEM] Persistent Token expired, API calling...");
+          return res
+            .status(400)
+            .json({ message: "Persistent Token has expired, please refresh it." });
+        } else {
+          return res.status(401).json({ message: "Invalid Persistent Token" });
+        }
+      }
+    } else {
+      return res.status(401).json({ message: "Invalid Session Token" });
+    }
+  }
 };
