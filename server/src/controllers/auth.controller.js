7ce5ca9cedb238ae366a8676891388f8fa171092
Trung4n <nguyentrungan4993@gmail.com>
2025-11-21 15:53:18 +0700

    refactor: improvement auth controller and service

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,39 +1,105 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
 import { AuthService } from "../services/auth.service.js";
-import { OtpService } from "../services/otp.service.js";
 import { MailService } from "../services/mail.service.js";
 import { TokenService } from "../services/token.service.js";
-import { env } from "../config/env.js";
+import { RedisService } from "../services/redis.service.js";
 
-export const register = asyncHandler(async (req, res) => {
+import { env } from "../config/env.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { AppError } from "../utils/AppError.js";
+import { generateOtpAndHash, generateToken } from "../utils/crypto.js";
+
+/**
+ * @route POST /auth/send-otp?type={register|forgot-password}
+ * validate request body and params (use validate.middleware.js)
+ * check user existence in database (use user.service.js):
+ * - if register: user should not exist
+ * - if forgot-password: user should exist
+ * store otp, token, user data in redis:
+ * - register: key = otp:<token>, value = {mail, pw, otpHash}
+ * - forgot-password: key = otp:<token>, value = {mail, otpHash}
+ * send otp email
+ * return token in response
+ */
+export const sendOTP = asyncHandler(async (req, res) => {
   // YOUR CODE HERE
-  // 1. Generate OTP
-  // 2. Store OTP and user in Redis with expiration
-  // 3. Send OTP to user's email
 });
 
+/**
+ * @route POST /auth/verify-otp?token={token}
+ * validate request body and params (use validate.middleware.js)
+ * get otp from redis using token (if not found, return error)
+ * compare otp from request with otpHash from redis (if not match, return error)
+ * return success response (no need to return user data)
+ */
+export const verifyOtp = asyncHandler(async (req, res) => {
+  // YOUR CODE HERE
+});
+
+/**
+ * @route POST /auth/register?token={token}
+ * validate request body and params (use validate.middleware.js)
+ * get user data from redis using token (if not found, return error)
+ * create user in database (use user.service.js)
+ * generate session and persistent tokens (use token.service.js)
+ * set cookies for session and persistent tokens
+ * return success response (no need to return user data)
+ */
+export const register = asyncHandler(async (req, res) => {
+  // YOUR CODE HERE
+});
+
+/**
+ * @route POST /auth/login
+ * validate request body (use validate.middleware.js)
+ * check user existence and password (use auth.service.js)
+ * create session and persistent tokens (use token.service.js)
+ * set cookies for session and persistent tokens
+ * return success response (no need to return user data)
+ */
 export const login = asyncHandler(async (req, res) => {
   // YOUR CODE HERE
 });
 
+/**
+ * @route POST /auth/change-password
+ * validate request body (use validate.middleware.js)
+ * get userId from token
+ * updateUser func (use user.service.js, compare current password, new password)
+ * return success response (no need to return user data)
+ */
 export const changePassword = asyncHandler(async (req, res) => {
   // YOUR CODE HERE
 });
 
+/**
+ * @route POST /auth/forgot-password
+ * validate request body (use validate.middleware.js)
+ * get mail and newpassword from request body
+ * updateUser func (use user.service.js)
+ * return success response (no need to return user data)
+ */
 export const forgotPassword = asyncHandler(async (req, res) => {
   // YOUR CODE HERE
 });
 
-export const verifyOtp = asyncHandler(async (req, res) => {
+/**
+ * @route POST /auth/logout
+ * get persistent token from cookies
+ * delete tokens from database (use token.service.js)
+ * clear cookies for session and persistent tokens
+ * return success response
+ */
+export const logout = asyncHandler(async (req, res) => {
   // YOUR CODE HERE
-  // 1. Verify OTP from Redis
-  // 2. If valid, create user account from stored data
-  // 3. Clear OTP and user data from Redis
 });
 
 export const refeshToken = asyncHandler(async (req, res) => {
   const tokenDoc = req.cookies.persistent;
-  const newPersistentToken = await TokenService.rotatePersistentToken(tokenDoc, req);
+  const newPersistentToken = await TokenService.rotatePersistentToken(
+    tokenDoc,
+    req.headers["user-agent"],
+    req.ip
+  );
   const newSessionToken = await TokenService.rotateSessionToken(tokenDoc.userId);
 
   res.cookie("session", newSessionToken, {
--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -2,16 +2,34 @@ import { User } from "../models/user.model.js";
 import { AppError } from "../utils/AppError.js";
 
 export const AuthService = {
-  async register({ mail, password }) {
-    // YOUR CODE HERE
-  },
   async login({ mail, password }) {
-    // YOUR CODE HERE
-  },
-  async changePassword({ userId, currentPassword, newPassword }) {
-    // YOUR CODE HERE
-  },
-  async forgotPassword({ mail }) {
-    // YOUR CODE HERE
+    const user = await User.findOne({ mail }).select("+password");
+    // check if the user not exists
+    if (!user) {
+      throw new AppError("Invalid email or password", 401);
+    }
+
+    // if user has been locked
+    if (user.status === "inactive") {
+      throw new AppError("Your account has been locked.", 403);
+    }
+
+    // compare password
+    if (await user.comparePassword(password)) {
+      user.failedLoginAttempts = user.failedLoginAttempts + 1;
+
+      if (user.failedLoginAttempts >= 5) {
+        user.status = "inactive";
+      }
+
+      await user.save();
+      throw new AppError("Invalid email or password", 401);
+    }
+
+    // reset failed login attempts
+    if (user.failedLoginAttempts > 0) {
+      user.failedLoginAttempts = 0;
+      await user.save();
+    }
   },
 };
