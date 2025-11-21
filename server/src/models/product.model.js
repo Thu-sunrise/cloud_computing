1e75767310646fa6a1758a42ba5994ab592b2944
Trung4n <nguyentrungan4993@gmail.com>
2025-11-21 14:10:32 +0700

    build: add schemas: cart, category, product,...

---
+++ server/src/models/product.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    createdBy: {
+      // FK
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Customer",
+      required: true,
+    },
+    categoryId: {
+      // FK
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      default: "",
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    salePrice: {
+      type: Number,
+      min: 0,
+    },
+    images: [
+      {
+        publicId: {
+          type: String,
+        },
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["active", "pending", "rejected", "deleted"],
+    },
+  },
+  { timestamps: true }
+);
+
+export const Product = mongoose.model("Product", productSchema);
