f5d6126f271b2065d4a42792b936644065519da5
Thu-sunrise <thu.hominh1704xx@hcmut.edu.vn>
2025-12-15 17:09:30 +0700

    update api review

--- server/src/controllers/review.controller.js
+++ server/src/controllers/review.controller.js
@@ -2,34 +2,42 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ReviewService } from "../services/review.service.js";
 import { AppError } from "../utils/AppError.js";
 
+/*
+  Get all reviews written by others for one product of user
+*/
 export const getReviewsOfProduct = asyncHandler(async (req, res) => {
   const { id } = await req.params;
   const data = await ReviewService.getReviewsOfProduct(id, req.query);
   // if not found
-  if (!data || data.length === 0) {
-    return res.status(404).json({
-      success: false,
-      message: "No reviews found",
-    });
+  if (!data || data.items.length === 0) {
+    return res.status(404).json({ success: false, message: "No reviews found" });
   }
   // if found -> return
   res.status(200).json({ success: true, data: data });
 });
 
+/*
+  create review
+*/
 export const createReviewToProduct = asyncHandler(async (req, res) => {
-  const reviewer = req.user.id;
-  const { rating, comment } = req.body;
-  const productId = req.params;
+  const reviewer = await req.user.sub;
+  const { rating, comment } = await req.body;
+  const { id: productId } = await req.params;
   const data = await ReviewService.createReview(reviewer, productId, rating, comment);
   res.json({ success: true, data: data });
 });
 
+/*
+  delete review (just only admin can delete)
+*/
 export const deleteReview = asyncHandler(async (req, res) => {
-  if (req.user.id !== req.params || req.user.role !== "admin") {
+  if (req.user.sub !== req.params && req.user.role !== "admin") {
     throw new AppError("Forbidden", 403);
   }
+  const { id: reviewId } = await req.params;
+
   // transfer the logic to the service
-  const result = await ReviewService.deleteReview();
+  const result = await ReviewService.deleteReview(reviewId);
   // return
   return res.status(200).json({
     success: true,
@@ -37,16 +45,22 @@ export const deleteReview = asyncHandler(async (req, res) => {
   });
 });
 
-export const updateReviewById = asyncHandler(async (req, res) => {
-  // check role
-  const data = req.body;
-  const { id } = req.params;
-  // transfer the logic to the service
-  const updateUser = await ReviewService.updateReview(id, data);
+/*
+    ------------------------------------------------------------------------
+    -------------PHAN NAY MINH THU CHUA TEST -> NE NO RA NHA----------------
+    ------------------------------------------------------------------------
+  */
 
-  return res.status(200).json({
-    success: true,
-    message: "Information updated successfully.",
-    data: updateUser,
-  });
+/*
+  Get all reviews written by others for the current user's products.
+*/
+export const getReviewsAllMyProducts = asyncHandler(async (req, res) => {
+  const { userId } = await req.params;
+  const data = await ReviewService.getReviewsAllMyProducts(userId, req.query);
+  // if not found
+  if (!data || data.items.length === 0) {
+    return res.status(404).json({ success: false, message: "No reviews found" });
+  }
+  // if found -> return
+  res.status(200).json({ success: true, data: data });
 });
--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -15,9 +15,8 @@ export const router = Router();
 // Example: api/product/list?page=1&limit=10&search=abc&sort=asc&category=electronics&priceMin=100&priceMax=1000
 // if query params not provided, use default values in controller (remcommended)
 
-
 router.get("/:id", getProduct);
- 
+
 router.post("/", requireAuth, upload.array("file"), createProduct);
 
 router.put("/:id", requireAuth, upload.array("file"), updateProduct);
