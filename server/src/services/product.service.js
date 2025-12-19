018da4e5fa3f34596f15d2e76cec18b5d83a9f88
Thu-sunrise <thu.hominh1704xx@hcmut.edu.vn>
2025-12-19 15:03:38 +0700

    ban demo

--- server/src/services/product.service.js
+++ server/src/services/product.service.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 import { Product } from "../models/product.model.js";
 import { Cart } from "../models/cart.model.js";
 import { CloudinaryService } from "./cloudinary.service.js";
-
-import { CloudinaryService } from "./cloudinary.service.js";
 import { AppError } from "../utils/AppError.js";
 
 export const ProductService = {
